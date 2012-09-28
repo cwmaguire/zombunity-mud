4872304d6d935095bd0e66d945b27488df8f06e6
Chris Maguire <cwmaguire@gmail.com>
2012-09-28 16:37:12 -0700

    Fixed "No Match" daemon tests when previous tests have left daemons registered

--- zombunity_server/test/zombunity/no_message_test.clj
+++ zombunity_server/test/zombunity/no_message_test.clj
@@ -6,15 +6,18 @@
 
 (def logging-in {"login_state" {:conn_id 1 :num_logins 1 :num_passwords 0}})
 
-(def not-logging-in {"msg" {:conn_id 1 :message "Command ABC not recognized."}})
+(def not-logging-in {"msg" {:conn-id 1 :message "Command ABC not recognized."}})
 
 (deftest user-logging-in
+  ; other tests may leave daemons registered
+  (reset! disp/daemon-fns {})
   (disp/register-daemon  (first (disp/filter-classpath-namespaces #"\.no.match$")))
 
   (data/set-target :zombunity.data.login-data/login)
   (reset! login-data/data {"login_state" {:conn_id 1 :num_logins 1 :num_passwords 0}})
 
   (disp/dispatch {:type nil :conn-id 1})
+  (disp/dispatch {:type "ABC" :conn-id 1})
 
   (is (= logging-in @login-data/data) "\"No Match\" daemon doesn't handle message when user is logging in"))
 
@@ -22,20 +25,21 @@
   (disp/register-daemon  (first (disp/filter-classpath-namespaces #"\.no.match$")))
 
   (data/set-target :zombunity.data.login-data/login)
+  (reset! login-data/data {})
 
-  (disp/dispatch {:type nil :conn-id 1})
+  (disp/dispatch {:type "ABC" :conn-id 1})
 
-  (is (= logging-in @login-data/data) "\"No Match\" handles message when user not logged in and not logging in"))
+  (is (= not-logging-in @login-data/data) "\"No Match\" handles message when user not logged in and not logging in"))
 
 (deftest user-logged-in
   (disp/register-daemon  (first (disp/filter-classpath-namespaces #"\.no.match$")))
 
   (data/set-target :zombunity.data.login-data/login)
-  (reset! login-data/data {"login_state" {:conn_id 1 :num_logins 1 :num_passwords 0}})
+  (reset! login-data/data {})
 
-  (disp/dispatch {:type nil :user-id 1 :conn-id 1})
+  (disp/dispatch {:type "ABC" :user-id 1 :conn-id 1})
 
-  (is (= logging-in @login-data/data) "\"No Match\" handles message when user is logged in"))
+  (is (= not-logging-in @login-data/data) "\"No Match\" handles message when user is logged in"))
 
 (deftest user-relogging-in
   (disp/register-daemon  (first (disp/filter-classpath-namespaces #"\.no.match$")))
